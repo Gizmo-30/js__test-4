ce1c3dc43b3ccb32c467fa55e4d97626964f5832
Gizmo-30 <anvaradizov80@gmail.com>
2021-11-24 16:46:09 +0500

    first

---
+++ script.js
@@ -0,0 +1,83 @@
+
+time = +prompt('Введите время в цифрах от 0 до 24')
+
+switch (time) {
+    case 0:
+        alert('Время 12 ночи')
+        break;
+    case 1:
+        alert('Время час ночи')
+        break;
+    case 2:
+        alert('Время 2 часа ночи')
+        break;
+    case 3:
+        alert('Время 3 часа утра')
+        break;
+    case 4:
+        alert('Время 4 часа утра')
+        break;
+    case 5:
+        alert('Время 5 часов утра')
+        break;
+    case 6:
+        alert('Время 6 часов утра')
+        break;
+    case 7:
+        alert('Время 7 часов утра')
+        break;
+    case 8:
+        alert('Время 8 часов утра')
+        break;
+    case 9:
+        alert('Время 9 часов утра')
+        break;
+    case 10:
+        alert('Время 10 часов утра')
+        break;
+    case 11:
+        alert('Время 11 часов утра')
+        break;
+    case 12:
+        alert('Время 12 часов утра')
+        break;
+    case 13:
+        alert('Время час дня')
+        break;
+    case 14:
+        alert('Время 2 часa дня')
+        break;
+    case 15:
+        alert('Время 3 часa дня')
+        break;
+    case 16:
+        alert('Время 4 часa дня')
+        break;
+    case 17:
+        alert('Время 5 часов дня')
+        break;
+    case 18:
+        alert('Время 6 часов дня')
+        break;
+    case 19:
+        alert('Время 7 часов вечера')
+        break;
+    case 20:
+        alert('Время 8 часов вечера')
+        break;
+    case 21:
+        alert('Время 9 часов ночи')
+        break;
+    case 22:
+        alert('Время 10 часов ночи')
+        break;
+    case 23:
+        alert('Время 11 часов ночи')
+        break;
+    case 24:
+        alert('Время 12 часов ночи')
+        break;
+    default:
+        alert('Такого времени суток не существует')
+        break;
+}
